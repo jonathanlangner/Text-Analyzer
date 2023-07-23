e5e1c22ed6ff194537ed5b3d3e56eaa9fdc4857a
jonathanlangner <jonlangner@gmail.com>
2023-07-23 03:06:12 -0500

    Preparing for Analysis
    Preparing for Analysis

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Text-Analyzer
 
 ## Primary Purpose: To analyze text for various values and metrics.
+
+
+
+### TODO: Add Node back in and chart.js
---
+++ text-analysis.js
@@ -0,0 +1,28 @@
+const logContent = function(){
+    console.log(textToParse.value);
+}
+
+// Word Count
+const numberOfWords = function(){
+
+}
+
+// Number of Characters
+const numberCharacters = function(){
+
+}
+
+// Number of Numerals
+const numberNumerals = function(){
+
+}
+
+// Length of the String
+const stringLength = function(){
+
+}
+
+
+const textToParse = document.querySelector("#to-parse");
+const parseTextBtn = document.querySelector("#myBtn").addEventListener("click", logContent); 
+console.log(textToParse);
